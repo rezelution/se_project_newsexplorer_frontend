08f10082b1da82ee108eff3d63efbcff807677bd
Alex Reznik <alex_reznik@verizon.net>
2025-08-04 13:34:20 -0400

    renamed project

--- newsexplorer_frontend/newsexplorer/src/components/App/App.jsx
+++ newsexplorer_frontend/newsexplorer/src/components/App/App.jsx
@@ -1,3 +1,17 @@
-function App() {}
+import { useState, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+
+import Header from "../Header/header";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page_content">
+        <header />
+      </div>
+    </div>
+  );
+}
 
 export default App;
---
+++ newsexplorer_frontend/newsexplorer/src/components/Header/header.jsx
@@ -0,0 +1,7 @@
+import "./header.css";
+
+function Header() {
+  return <div></div>;
+}
+
+export default Header;
