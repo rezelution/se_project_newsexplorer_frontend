e27df204a7b7984839a8a06bc7cc41706dd82db4
Alex Reznik <alex_reznik@verizon.net>
2025-08-23 09:43:36 -0400

    Update basename again

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/se_project_newsexplorer">
+    <BrowserRouter basename="/se_project_newsexplorer_frontend">
       <App />
     </BrowserRouter>
   </React.StrictMode>
