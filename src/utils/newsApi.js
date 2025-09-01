e7ab28b89f54c134a1fccc4b760471475a224654
Alex Reznik <alex_reznik@verizon.net>
2025-09-01 17:09:16 -0400

    updated api headers

--- src/utils/newsApi.js
+++ src/utils/newsApi.js
@@ -1,5 +1,12 @@
 import apiErrorMessages from "./apiErrorMessages";
 
+const newsApiBaseUrl =
+  import.meta.env.MOD === "production"
+    ? "https://nomoreparties.co/news/v2/everything"
+    : "https://newsapi.org/v2/everything";
+
+const APIkey = import.meta.env.VITE_NEWS_API_KEY;
+
 export const getNewsArticles = ({ searchTerm, APIkey }) => {
   const getFormattedDateNDaysAgo = (n) => {
     const date = new Date();
@@ -11,7 +18,7 @@ export const getNewsArticles = ({ searchTerm, APIkey }) => {
   const toDate = new Date().toISOString().split("T")[0];
 
   const url =
-    "https://newsapi.org/v2/everything?" +
+    `${newsApiBaseUrl}?` +
     "q=" +
     encodeURIComponent(searchTerm) +
     "&" +
@@ -22,11 +29,13 @@ export const getNewsArticles = ({ searchTerm, APIkey }) => {
     toDate +
     "&" +
     "sortBy=popularity&" +
-    "pageSize=100&" +
-    "apiKey=" +
-    APIkey;
+    "pageSize=100";
 
-  const req = new Request(url);
+  const req = new Request(url, {
+    headers: {
+      "X-Api-Key": APIkey,
+    },
+  });
 
   return fetch(req)
     .then((response) => {
@@ -49,14 +58,12 @@ export const getNewsArticles = ({ searchTerm, APIkey }) => {
     });
 };
 
-// formatting date helper function
 const formatDate = (isoDateString) => {
   const date = new Date(isoDateString);
   const options = { year: "numeric", month: "long", day: "numeric" };
   return date.toLocaleDateString(undefined, options);
 };
 
-// filter article data based on needed fields
 export const filterNewsArticles = (data) => {
   return data.articles.map((article) => ({
     urlToImage: article.urlToImage,
